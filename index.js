7b6f50a9835d787781cb194c711a8867f3a0d523
Afra <propazaman12@gmail.com>
2024-02-05 17:55:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const CAKE_ORDERED = 'CAKE_ORDERED';
+
+function orderCake(){
+  return  {
+        type: CAKE_ORDERED,
+        quantity: 1,
+    }
+}
+
+const initialState = {
+    numOfCakes: 10,
+}
+
+// (previousState, action) => newState
+
+const reducer = (state= initialState, action) =>{
+    switch(action.type){
+        case CAKE_ORDERED:
+            return{
+                numOfCakes: state.numOfCakes - 1,
+            }
+            default:
+                return state
+    }
+}
