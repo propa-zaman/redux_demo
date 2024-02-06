d0662c71cf23708bfa7e23c6ab9dd534d47d4065
Afra <propazaman12@gmail.com>
2024-02-06 10:50:23 +0600

    same reducer function for separate item

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bindActionCreators = redux.bindActionCreators //to pass action objects as 
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED';
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED';
+
 
 function orderCake() {
     return {
@@ -20,8 +23,23 @@ function restockCake(qty = 1) {
     }
 }
 
+function orderIceCream(qty = 1) {
+    return {
+        type: ICECREAM_ORDERED,
+        payload: qty,
+    }
+}
+
+function restockIceCream(qty = 1) {
+    return {
+        type: ICECREAM_RESTOCKED,
+        payload: qty,
+    }
+}
+
 const initialState = {
     numOfCakes: 10,
+    numOfIceCreams: 20,
 }
 
 // (previousState, action) => newState
@@ -40,6 +58,18 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes + action.payload,
             }
+            case ICECREAM_ORDERED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams - 1,
+            }
+
+
+        case ICECREAM_RESTOCKED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams + action.payload,
+            }
         default:
             return state
     }
@@ -55,11 +85,16 @@ const unsubscribe = store.subscribe(() => console.log('pdate state', store.getSt
 // store.dispatch(orderCake())
 // store.dispatch(restockCake(6))
 
-const actions = bindActionCreators({orderCake, restockCake}, store.dispatch)
+const actions = bindActionCreators({orderCake, restockCake, orderIceCream, restockIceCream}, store.dispatch)
 
 actions.orderCake();
 actions.orderCake();
 actions.orderCake();
 actions.restockCake(3);
+
+actions.orderIceCream();
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(3);
 
 unsubscribe() //unregister listener 
